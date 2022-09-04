9b65cf4f3cd55589c0e8d831d398e8de3c029c1c
Charles Cantkier <charles.cantkier@gmail.com>
2022-09-04 15:52:54 -0500

    aaa

--- app.gd
+++ app.gd
@@ -1,16 +1,42 @@
 extends Control
 
+# 0 = Brown Noise
+# 1 = Filt. Brown Noise
+# 2 = Filt. White Noise
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var NoiseType # what noise has been set
+onready var dropdown = $OptionButton # the dropdown menu
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	NoiseType = 0
+
+func _process(delta):
+	if NoiseType == 0:
+		$StaticBrown.play()
+	if NoiseType == 1:
+		$StaticBrownFilt.play()
+	if NoiseType == 2:
+		$StaticFilt.play()
+
+
+func _on_OptionButton_item_selected(index):
+	var selc = index
+	
+	if selc == 0:
+		NoiseType = 0
+	if selc == 1:
+		NoiseType = 1
+	if selc == 2:
+		NoiseType = 2
+
+
+func _on_Stop_button_up():
+	$StaticBrown.stop()
+	$StaticBrownFilt.stop()
+	$StaticFilt.stop()
+	
+	
+
+
+func _on_Play_button_up():
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
